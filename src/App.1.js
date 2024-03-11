2105f5430eac906aa79d660aaaca61830e00d31e
bhukyarajugithub <RAJUBHUKYA3429@GMAIL.COM>
2024-03-12 00:26:12 +0530

    changes

--- src/App.1.js
+++ src/App.1.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
     return ( <
@@ -12,17 +11,16 @@ function App() {
         alt = "logo" / >
         <
         p >
-        my first code and save to reload. <
-        /p> <
+        Edit < code > src / App.js < />code> and save to reload. < / >
+        p > <
         a className = "App-link"
         href = "https://reactjs.org"
         target = "_blank"
         rel = "noopener noreferrer" >
-        Learn React <
-        /a> < /
-        header > <
-        /div>
+        Learn React < />a> < / >
+        header > < />div>
     );
 }
 
-export default App;
+export default App; < /></ > < /></ > < />);
+}
